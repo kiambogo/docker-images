878f9d0ac40f4b7debe6c24a5050aa1a0c9498bd
Christopher Poenaru <kiambogo@gmail.com>
2017-04-06 22:54:38 -0400

    seemingly working trsp routing

--- postgres_pgrouting/dependencies/pgrouting.sql
+++ postgres_pgrouting/dependencies/pgrouting.sql
@@ -25,37 +25,75 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TYPE shortest_route AS (distance double precision, route varchar(2000));
 CREATE OR REPLACE FUNCTION shortest_distance_route(start_lon double precision, start_lat double precision, end_lon double precision, end_lat double precision)
-RETURNS text AS $$
+RETURNS TABLE (seq integer, points text, cost float8) AS $$
 DECLARE
 	route text;
 BEGIN
-	SELECT ST_AsEncodedPolyline(
-		ST_SetPoint(
-			ST_SetPoint(
-				(SELECT ST_MakeLine(ways.the_geom) FROM pgr_trsp('SELECT gid::integer as id, source::integer, target::integer, cost::float FROM ways',
+  RETURN QUERY
+				SELECT a.seq, st_astext((ST_DumpPoints(roads.way)).geom), sum(a.cost) as distance from pgr_trsp('SELECT id::integer, source::integer, target::integer, distance::float8 as cost FROM planet_osm_line_noded',
 						(SELECT id from closest_road_to_point(start_lon, start_lat))::integer,
 						(SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(start_lon, start_lat)), (SELECT ST_GeometryFromText('POINT('||start_lon||' '||start_lat||')',4326)))),
 						(SELECT id from closest_road_to_point(end_lon, end_lat))::integer,
 						(SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(end_lon, end_lat)), (SELECT ST_GeometryFromText('POINT('||end_lon||' '||end_lat||')',4326)))),
-						true, false) INNER JOIN ways on ways.gid = id2),
-				0,
-				(ST_LineInterpolatePoint(
-						(SELECT the_geom from ways where gid = (SELECT id from closest_road_to_point(start_lon, start_lat))),
-						(SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(start_lon, start_lat)), (SELECT ST_GeometryFromText('POINT('||start_lon||' '||start_lat||')',4326))))
-				))
-		),
-		-1,
-		(ST_LineInterpolatePoint(
-				(SELECT the_geom from ways where gid = (SELECT id from closest_road_to_point(end_lon, end_lat))),
-				(SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(end_lon, end_lat)), (SELECT ST_GeometryFromText('POINT('||end_lon||' '||end_lat||')',4326))))
-		))
-)
-		) INTO route;
-		RETURN route;
+						true, false) as a INNER JOIN planet_osm_line_noded roads on roads.id = a.id2 group by a.seq, roads.way order by a.seq asc;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION shortest_distance_route2(start_lon double precision, start_lat double precision, end_lon double precision, end_lat double precision)
+RETURNS TABLE (points text) AS $$
+DECLARE
+	route text;
+BEGIN
+  RETURN QUERY
+  SELECT st_astext(
+    (ST_dumppoints(
+        ST_SetPoint(
+          ST_SetPoint(
+            (SELECT ST_MakeLine(ways.way) FROM pgr_trsp('SELECT id::integer, source::integer, target::integer, distance::float8 as cost FROM planet_osm_line_noded',
+                (SELECT id from closest_road_to_point(start_lon, start_lat))::integer,
+                (SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(start_lon, start_lat)), (SELECT ST_GeometryFromText('POINT('||start_lon||' '||start_lat||')',4326)))),
+                (SELECT id from closest_road_to_point(end_lon, end_lat))::integer,
+                (SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(end_lon, end_lat)), (SELECT ST_GeometryFromText('POINT('||end_lon||' '||end_lat||')',4326)))),
+                true, false) as a INNER JOIN planet_osm_line_noded as ways on ways.id = id2 where a.seq <> 1 and a.id2 <> ((SELECT id from closest_road_to_point(end_lon, end_lat))::integer)),
+            0,
+            (ST_LineInterpolatePoint(
+                (SELECT way from planet_osm_line_noded where id = (SELECT id from closest_road_to_point(start_lon, start_lat))),
+                (SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(start_lon, start_lat)), (SELECT ST_GeometryFromText('POINT('||start_lon||' '||start_lat||')',4326))))
+            ))
+          ),
+          -1,
+          (ST_LineInterpolatePoint(
+              (SELECT way from planet_osm_line_noded where id = (SELECT id from closest_road_to_point(end_lon, end_lat))),
+              (SELECT ST_LineLocatePoint((SELECT road from closest_road_to_point(end_lon, end_lat)), (SELECT ST_GeometryFromText('POINT('||end_lon||' '||end_lat||')',4326))))
+          ))
+        )
+    )).geom
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 CREATE OR REPLACE FUNCTION vertex_from_point(lo double precision, la double precision)
 RETURNS integer AS $$
@@ -94,10 +132,10 @@ DECLARE
   road closest_road;
 BEGIN
   RETURN QUERY
-    SELECT gid as id
-    , ST_Distance(ST_GeomFromText('POINT('||lon||' '||lat||')',4326), the_geom) AS distance
-    , the_geom as road
-  FROM ways
+    SELECT planet_osm_line_noded.id
+    , ST_Distance(ST_GeomFromText('POINT('||lon||' '||lat||')',4326), way) AS distance
+    , way as road
+  FROM planet_osm_line_noded
   ORDER BY distance ASC
   LIMIT 1;
 END;
